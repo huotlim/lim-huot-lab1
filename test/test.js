bc975c888fa394f9d3934e2cc908ccae7c600441
limhuot <lim.hout9988@gmail.com>
2025-10-31 10:54:54 +0700

    update new v6

--- test/test.js
+++ test/test.js
@@ -1,12 +1,20 @@
-const request = require('supertest');
-import chai from 'chai';
-
-const app = require('../app');
+// test/test.js
+const chai = require('chai');
 const { expect } = chai;
-describe('GET /', () => {
- it('should return Hello, GitHub Actions!', async () => {
- const res = await request(app).get('/');
- expect(res.status).to.equal(200);
- expect(res.text).to.equal('Hello, GitHub Actions!');
- });
+const request = require('supertest');
+const express = require('express');
+
+describe('Sample API test', function () {
+  let app;
+
+  before(function () {
+    app = express();
+    app.get('/hello', (req, res) => res.send('Hello World'));
+  });
+
+  it('GET /should return Hello, GitHub Actions!', async function () {
+    const res = await request(app).get('/hello');
+    expect(res.status).to.equal(200);
+    expect(res.text).to.equal('Hello World');
+  });
 });
